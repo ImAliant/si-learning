69c0cb7787827e603f47a790b9dcbf32c25cb937
ImAliant <111398111+ImAliant@users.noreply.github.com>
2024-12-23 02:59:53 +0100

    add navigation graph, top bar and bottom bar

--- app/src/main/java/fr/diamant/silearning/MainActivity.kt
+++ app/src/main/java/fr/diamant/silearning/MainActivity.kt
@@ -1,16 +1,29 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package fr.diamant.silearning
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.compose.rememberNavController
 import fr.diamant.silearning.ui.theme.SILearningTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +32,54 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SILearningTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                MainScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun MainScreen() {
+    val navController = rememberNavController()
+
+    Scaffold(topBar = { TopBar() },
+        bottomBar = { BottomBar(navController) }
+    ) { innerPadding ->
+        NavigationGraph(navController, Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun TopBar() = CenterAlignedTopAppBar(title = {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = LocalContext.current.getString(R.string.app_name),
+        style = MaterialTheme.typography.displayMedium
     )
+})
+
+@Composable
+fun BottomBar(navController: NavController) {
+    val items = listOf(
+        NavigationDestinations.Home,
+        NavigationDestinations.Settings
+    )
+
+    NavigationBar {
+        items.forEach { destination ->
+            NavigationBarItem(
+                selected = navController.currentBackStackEntry?.destination?.hierarchy?.any { it.route == destination.route } == true,
+                onClick = { navController.navigate(destination.route) },
+                label = { Text(text = destination.route.replaceFirstChar { it.uppercase() }) },
+                icon = { Icon(Icons.Default.Home, contentDescription = destination.route) }
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun MainScreenPreview() {
     SILearningTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
--- app/src/main/java/fr/diamant/silearning/NavigationGraph.kt
+++ app/src/main/java/fr/diamant/silearning/NavigationGraph.kt
@@ -1,2 +1,28 @@
 package fr.diamant.silearning
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+
+@Composable
+fun NavigationGraph(navController: NavHostController, modifier: Modifier = Modifier) {
+    NavHost(
+        navController = navController,
+        startDestination = NavigationDestinations.Home.route,
+        modifier = modifier
+    ) {
+        composable(NavigationDestinations.Home.route) {
+            //HomeScreen()
+        }
+        composable(NavigationDestinations.Settings.route) {
+            //SettingsScreen()
+        }
+    }
+}
+
+sealed class NavigationDestinations(val route: String) {
+    data object Home : NavigationDestinations("home")
+    data object Settings : NavigationDestinations("settings")
+}
