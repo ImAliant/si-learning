eb46a156661815baf562caea945c85187f89aeb1
ImAliant <111398111+ImAliant@users.noreply.github.com>
2024-12-27 02:18:19 +0100

    simple ui for game screen

--- app/src/main/java/fr/diamant/silearning/ui/game/GameScreen.kt
+++ app/src/main/java/fr/diamant/silearning/ui/game/GameScreen.kt
@@ -1,22 +1,30 @@
 package fr.diamant.silearning.ui.game
 
+import android.content.Context
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.material3.Card
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import fr.diamant.silearning.R
 import fr.diamant.silearning.viewmodel.game.GameViewModel
-import java.util.logging.Logger
 
 @Composable
 fun GameScreen(
@@ -25,38 +33,83 @@ fun GameScreen(
     categoryId: Int,
     model: GameViewModel = viewModel()
 ) {
-    //val userAnswer by model.userAnswer
-    //val currentQuestion = remember { model.currentQuestion }
-    //val context = LocalContext.current
-
-    /*Column {
-        // Show the current question
-        Card {
-            Text(text = currentQuestion.question)
-        }
-
-        // Textfield for the user to input their answer
-        OutlinedTextField(
-            value = userAnswer,
-            onValueChange = model::updateUserAnswer,
-            label = { Text(text = context.getString(R.string.answer_tf)) }
-        )
-    }*/
+    val current = model.currentQuestion?.question
+    val userAnswer by remember { model.userAnswer }
+    val context = LocalContext.current
 
     model.initializeGame(categoryId)
 
-    Text(text = model.currentQuestion?.question ?: "No question found")
+    Column(
+        modifier = Modifier.fillMaxSize().padding(paddingValues),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Spacer(modifier = Modifier.height(16.dp))
+
+        QuestionText(question = current)
+
+        AnswerInput(
+            userAnswer = userAnswer,
+            context = context,
+            onAnswerChange = model::updateUserAnswer
+        )
+
+        ActionButtons(
+            onCheckClick = { model.checkAnswer(navController) },
+            onNextClick = { model.nextQuestion(navController) },
+            checkEnabler = { userAnswer.isNotEmpty() },
+            checkText = context.getString(R.string.check_btn),
+            nextText = context.getString(R.string.next_btn)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+    }
 }
 
-/*@Composable
-private fun ShowQuestions(model: GameViewModel, categoryId: Int) {
-    model.initializeGame(categoryId)
+@Composable
+private fun QuestionText(question: String?) {
+    Text(
+        text = question ?: "No question found",
+        textAlign = TextAlign.Center,
+        modifier = Modifier.padding(horizontal = 16.dp)
+    )
+}
 
-    val questions = remember { model.questions }
+@Composable
+private fun AnswerInput(userAnswer: String, context: Context, onAnswerChange: (String) -> Unit) {
+    OutlinedTextField(
+        value = userAnswer,
+        onValueChange = onAnswerChange,
+        label = { Text(text = context.getString(R.string.answer_tf)) },
+        modifier = Modifier.padding(horizontal = 16.dp)
+    )
+}
 
-    LazyColumn {
-        itemsIndexed(questions) { _, question ->
-            Text(text = question.question)
+@Composable
+private fun ActionButtons(
+    onCheckClick: () -> Unit,
+    onNextClick: () -> Unit,
+    checkEnabler: () -> Boolean = { true },
+    nextEnabler: () -> Boolean = { true },
+    checkText: String,
+    nextText: String
+) {
+    Row(
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = Modifier.fillMaxWidth().padding(8.dp)
+    ) {
+        Button(
+            onClick = onCheckClick,
+            enabled = checkEnabler()
+        ) {
+            Text(text = checkText)
+        }
+
+        Button(
+            onClick = onNextClick,
+            enabled = nextEnabler()
+        ) {
+            Text(text = nextText)
         }
     }
-}*/
+}
--- app/src/main/java/fr/diamant/silearning/viewmodel/game/GameViewModel.kt
+++ app/src/main/java/fr/diamant/silearning/viewmodel/game/GameViewModel.kt
@@ -3,22 +3,23 @@ package fr.diamant.silearning.viewmodel.game
 import android.app.Application
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.NavController
 import fr.diamant.silearning.SILearningApplication
 import fr.diamant.silearning.data.entity.Question
+import fr.diamant.silearning.navigation.NavigationDestinations
 import kotlinx.coroutines.launch
 import java.util.logging.Logger
 
 class GameViewModel(application: Application): AndroidViewModel(application) {
     private val _container = (application as SILearningApplication).container
     private var currentQuestionIndex = 0
+    private var isGameStarted = mutableStateOf(false)
     var currentQuestion: Question? = null
     private var questions = mutableStateListOf<Question>()
-    var isGameStarted = mutableStateOf(false)
 
-    //var userAnswer = mutableStateOf("")
+    var userAnswer = mutableStateOf("")
 
     fun initializeGame(categoryId: Int) {
         viewModelScope.launch {
@@ -51,16 +52,26 @@ class GameViewModel(application: Application): AndroidViewModel(application) {
         }
     }
 
-    fun nextQuestion() {
+    fun checkAnswer(navController: NavController) {
+        if (currentQuestion?.answer == userAnswer.value) {
+            Logger.getLogger("GameViewModel").info("Correct answer")
+        } else {
+            Logger.getLogger("GameViewModel").warning("Wrong answer")
+        }
+        nextQuestion(navController)
+    }
+
+    fun nextQuestion(navController: NavController) {
         currentQuestionIndex++
         if (currentQuestionIndex < questions.size) {
             currentQuestion = questions[currentQuestionIndex]
         } else {
-            Logger.getLogger("GameViewModel").warning("No more questions")
+            val destination = NavigationDestinations.Home.route
+            navController.navigate(destination)
         }
     }
 
-    /*fun updateUserAnswer(answer: String) {
+    fun updateUserAnswer(answer: String) {
         userAnswer.value = answer
-    }*/
+    }
 }
